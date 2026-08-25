875e2e21598d75ba37da5418a48976780156b3af
Carter <carter461954@gmail.com>
2026-08-25 10:59:22 +0300

    ╨Ю╨╜╨╛╨▓╨╗╨╡╨╜╨╜╤П ╤Б╨░╨╣╤В╤Г

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,7 @@
 @echo off
 cd /d D:\Project\starfamily
-echo Збираємо проект...
-npm run build
-if %errorlevel% neq 0 (
-    echo BUILD FAILED - deploy скасовано
-    pause
-    exit /b 1
-)
-echo Build успішний!
 git add -A
-set /p msg="Введи коментар до змін: "
-git commit -m "%msg%"
-if %errorlevel% neq 0 (
-    echo Немає змін для коміту
-    pause
-    exit /b 0
-)
+git commit -m "Оновлення сайту"
 git push
-echo.
-echo Готово! Vercel задеплоїть автоматично.
+echo Готово!
 pause
